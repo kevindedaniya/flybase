098560f1c15be55b5f4750d8191c73fe8ddacad6
kevin dedaniya <kevin.dedaniya@gmail.com>
2020-07-31 15:07:42 +0530

    ✨ add Home page in App.js

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Playarea from "components/Playarea/Playarea";
+
+class Home extends React.Component {
+	render() {
+		return(
+			<Playarea />
+		);
+	}
+}
+
+export default Home;
